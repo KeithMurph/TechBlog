d54f9bd2578d670c924acdb5151662fee75a2865
KeithMurph <keithmurphy94@gmail.com>
2021-08-19 02:51:47 -0700

    heroku bugsss

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,9 +5,9 @@ const postRoutes = require("./postRoutes")
 const commentRoutes = require("./commentRoutes")
 
 
-// router.use("/users",userRoutes)
-// router.use("/posts",postRoutes)
-// router.use("/comments",commentRoutes)
+router.use("/users",userRoutes)
+router.use("/posts",postRoutes)
+router.use("/comments",commentRoutes)
 
 
 module.exports = router;
